555f2abf9b6082874a1a51a88935d82cc1b6e236
Francesca Pietrobon <francesca13pietrobon@gmail.com>
2022-02-09 21:54:04 +0100

    Added base face detection

--- main.cpp
+++ main.cpp
@@ -3,24 +3,36 @@
 #include <iostream>
 #include <string>
 
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+
 #include "my_functions.h"
 
-using namespace cv;
 using namespace std;
+using namespace cv;
+using namespace dlib;
 
 
 int main(int argc, char* argv[])
 {
 
     // Load SNN
-    
-    string weights = "/home/fra/Project/Models/Frozen_graph_All_final_net_5e-4.pb";
+    string weights = "/home/fra/PROGETTO_PACS/Face-Anti-Spoofing/models/Frozen_graph_All_final_net_5e-4.pb";
     dnn::Net cvNet = cv::dnn::readNetFromTensorflow(weights);
 
     // Load ML Model
-    Ptr<ml::RTrees> svm = Algorithm::load<ml::RTrees> ("/home/fra/Project/Models/All_RF_opencv_final_net_lr5e-4.xml");
+    Ptr<ml::RTrees> svm = Algorithm::load<ml::RTrees> ("/home/fra/PROGETTO_PACS/Face-Anti-Spoofing/models/All_RF_opencv_final_net_lr5e-4.xml");
+
+    // Load face detection and pose estimation models.
+    frontal_face_detector detector = get_frontal_face_detector();
+    
 
     Mat img;
+    
     string window_name = "Image selected";
 
     if (argc > 2) // If there is a path for the image take it
@@ -38,6 +50,8 @@ int main(int argc, char* argv[])
                     img = imread(img_path, IMREAD_COLOR);
                     namedWindow(window_name); //create a window
                     imshow(window_name, img);
+                    
+                    face_detection(detector, img);
 
                     string output = make_prediction(img, cvNet, svm);
                     setWindowTitle(window_name, output);
@@ -82,6 +96,8 @@ int main(int argc, char* argv[])
 
             imshow(window_name, frame);
 
+            face_detection(detector, frame);
+
             string output = make_prediction(frame, cvNet, svm);
             setWindowTitle(window_name, output);
 
@@ -92,7 +108,7 @@ int main(int argc, char* argv[])
             //If the 'Esc' key is pressed, break the while loop.
             //If the any other key is pressed, continue the loop 
             //If any key is not pressed withing 100 ms, continue the loop 
-            if (waitKey(1) == 27)
+            if (waitKey(100) == 27)
             {
                 cout << "Esc key is pressed by user. Stoppig the video" << endl;
                 break;
--- my_functions.h
+++ my_functions.h
@@ -1,15 +1,25 @@
+#ifndef MY_FUNCTIONS_H
+#define MY_FUNCTIONS_H
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn/dnn.hpp>
 #include <iostream>
 #include <string>
 
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+
 using namespace cv;
 using namespace std;
+using namespace dlib;
 
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
 
 string make_prediction(Mat img, dnn::Net cvNet, Ptr<ml::RTrees> svm);
 
+void face_detection(frontal_face_detector detector, Mat img);
 
 #endif
